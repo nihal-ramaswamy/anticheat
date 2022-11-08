0c4d2db02597c9484e1177f3aa7f5117ce0170a7
Nihal Ramaswamy <nihalramaswamy@gmail.com>
2022-11-08 23:51:52 +0530

    Cleaned auth db

--- src/db/auth.db.ts
+++ src/db/auth.db.ts
@@ -1,37 +1,13 @@
 import {
   getAuth,
-  createUserWithEmailAndPassword,
   setPersistence,
   browserSessionPersistence,
-  GoogleAuthProvider,
-  signInWithPopup,
   signInWithEmailAndPassword,
   signOut
 } from "firebase/auth";
 import { app } from "./firebase.db";
 
 const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-const _signUpWithEmailPassword = async (email: string, password: string) => {
-  try {
-    const res = await createUserWithEmailAndPassword(auth, email, password);
-    return { status: "ok", data: res.user };
-  } catch (e) {
-    return { status: "error", error: e };
-  }
-};
-
-// Sets persistence while signing up with email and password
-export const signUpWithEmailPassword = async (email: string, password: string) => {
-  try {
-    await setPersistence(auth, browserSessionPersistence);
-    return await _signUpWithEmailPassword(email, password);
-  } catch (e) {
-    return { status: "error", error: e };
-  }
-};
-
 
 const _signInWithEmailPassword = async (email: string, password: string) => {
   try {
@@ -51,28 +27,6 @@ export const signInWithEmailPassword = async (email: string, password: string) =
     return { status: "error", error: e };
   }
 }
-
-const _signInWithGoogle = async () => {
-  try {
-    const res = await signInWithPopup(auth, provider);
-    const credentials = GoogleAuthProvider.credentialFromResult(res);
-    return { status: "ok", data: { "credentials": credentials, "user": res.user } };
-  } catch (e: any) {
-    const credentials = GoogleAuthProvider.credentialFromError(e);
-    return { status: "error", error: { credentials, e } };
-  }
-};
-
-// Sets Persistence while signing in with google
-export const signInWithGoogle = async () => {
-  try {
-    await setPersistence(auth, browserSessionPersistence);
-    return await _signInWithGoogle();
-  } catch (e) {
-    return { status: "error", error: e };
-  }
-};
-
 
 export const signOutOfSession = async () => {
   try {
